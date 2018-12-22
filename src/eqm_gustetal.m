83031f61b4c2b3bf152c4cca409015f588399427
Emily Martell <eemartell@email.wm.edu>
2018-12-22 10:25:06 -0500

    Write eqm_gustetal in terms of V equations

--- src/eqm_gustetal.m
+++ src/eqm_gustetal.m
@@ -21,45 +21,52 @@ in = state(4);  	%Notional interest rate last period
 for icol = 1:Rdim   
     % Policy Function Guesses
 % Put V functions here instead
-    cp = x(1,icol);     %Consumption policy current period 
-    pigap = x(2,icol);	%Inflation policy current period     
+    Vlambdap = x(1,icol); %Vlambda policy current period
+    Vpip = x(2,icol);     % Vpi policy current period
+    %cp = x(1,icol);     %Consumption policy current period 
+    %pigap = x(2,icol);	%Inflation policy current period     
     %----------------------------------------------------------------------
     % Solve for variables
     %----------------------------------------------------------------------
 % solve for other time t variables, in particular c and pigap, given state and the V functions
-% with goal of updating the state variables 
+% with goal of updating the state variables
+    lam = 1/Vlambdap;
+    c = lam;
+    pigap = (1+sqrt((P.varphi + 4*Vpip)/P.varphi))/2;
     % Aggregate resource constraint (6)    
-    y = cp/(1-P.varphi*(pigap-1)^2/2);
+    y = c/(1-P.varphi*(pigap-1)^2/2);
     % Interest rate rule (8)
     inp = in^P.rhoi*(S.i*pigap^P.phipi)^(1-P.rhoi)*exp(mp);
-    
     if P.zlbflag
         i = max(1,inp);
     else
         i = inp;
     end
     % FOC Labor (1,2)
-    lam = cp;
     w = S.chi*y^P.eta*lam;
     %----------------------------------------------------------------------
     % Linear interpolation of the policy variables
     %----------------------------------------------------------------------
 % interpolate V functions (instead of c and pigap) at updated state
-    if ~EEflag 
-        [cppArr3,pigappArr3] = Fallterp423_R(...
+    if ~EEflag         
+        %[cppArr3,pigappArr3] = Fallterp423_R(...
+        [VlambdapArr3,VpipArr3] = Fallterp423_R(...
             O.g_pts,O.s_pts,O.mp_pts,O.in_pts,...
             G.in_grid,...
             inp,...
-            pf.c,pf.pigap);
+            pf.Vlambda,pf.Vpi);
+            %pf.c,pf.pigap);
     else
         % Growth rate
         gpVec = ((1-P.rhog)*P.g + P.rhog*g + GH.e_nodes);
         % Preferences 
         spVec = ((1-P.rhos)*P.s + P.rhos*s + GH.u_nodes);
-        [cppArr3,pigappArr3] = allterp423(...
+        %[cppArr3,pigappArr3] = allterp423(...
+        [VlambdapArr3,VpipArr3] = allterp423(...
                                 G.g_grid,G.s_grid,G.mp_grid,G.in_grid,...
                                 gpVec,spVec,GH.v_nodes,inp,...
-                                pf.c,pf.pigap);
+                                pf.Vlambda,pf.Vpi);
+                                %pf.c,pf.pigap);
         gpArr3 = gpVec(:,ones(GH.shockpts,1),ones(GH.shockpts,1));
     end
 
@@ -68,10 +75,12 @@ for icol = 1:Rdim
     %---------------------------------------------------------------------- 
 % replicate the update at time t for t+1 variables instead only using the
 % equations you need to get variables to evaulate the expectation operators
+    lampArr3 = VlambdapArr3;
+    cppArr3 = lampArr3;
+    pigappArr3 = (1+sqrt((P.varphi + 4*VpipArr3)/P.varphi))/2;
     % Aggregate resource constraint  
     ypArr3 = cppArr3./(1-P.varphi*(pigappArr3-1).^2/2);
     % Stochastic discount factor
-    lampArr3 = cppArr3;
     sdfArr3 = P.beta*lam./lampArr3;
     %----------------------------------------------------------------------
     % Numerical integration
