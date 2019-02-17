6d7c689bc92c8d77aba65da3bce9a9c97d4875fb
Emily Martell <eemartell@email.wm.edu>
2019-02-17 10:41:29 -0500

    Grab clean eqm.m copy

--- src/general/eqm.m
+++ src/general/eqm.m
@@ -12,7 +12,7 @@ in = state(4);          %Notional interest rate last period
 c = state(5);           %Consumption last period
 k = state(6);           %Capital last period
 x = state(7);           %Investment last period   
-%w = state(8);           %Real wage last period
+w = state(8);           %Real wage last period
 
 for icol = 1:ncol
     % Policy Function Guesses
@@ -20,24 +20,24 @@ for icol = 1:ncol
     pigap = pf0(2,icol);   %Inflation gap current period  
     n = pf0(3,icol);       %Labor current period 
     q = pf0(4,icol);       %Tobin's q current period
-    %ups = pf0(5,icol);     %Utilization current period
+    ups = pf0(5,icol);     %Utilization current period
     %----------------------------------------------------------------------
     % Current period
     %----------------------------------------------------------------------
     % HH FOC utilization (1)
-    %rk = S.rk*exp(P.sigups*(ups-1));
+    rk = S.rk*exp(P.sigups*(ups-1));
     % Production function (2)
-    yf = (k/g)^P.alpha*n^(1-P.alpha); 
+    yf = (ups*k/g)^P.alpha*n^(1-P.alpha); 
     % Utilization definition (3)
-    %u = S.rk*(exp(P.sigups*(ups-1))-1)/P.sigups;
+    u = S.rk*(exp(P.sigups*(ups-1))-1)/P.sigups;
     % Firm FOC capital (4)
-    mc = rk*k/(P.alpha*g*yf);
+    mc = rk*ups*k/(P.alpha*g*yf);
     % Firm FOC labor (5)
     wp = (1-P.alpha)*mc*yf/n;
     % Real wage growth gap (6)
-    %wg = pigap*g*wp/(P.g*w);
+    wg = pigap*g*wp/(P.g*w);
     % Output definition (7)
-    yp = (1-P.varphi*(pigap-1)^2/2)*yf;
+    yp = (1-P.varphip*(pigap-1)^2/2-P.varphiw*(wg-1)^2/2)*yf - u*k/g;
     % Lagged ARC (13)
     y = c + x;
     % Output growth gap (8)
@@ -56,33 +56,35 @@ for icol = 1:ncol
     % Investment growth gap (14)
     xg = g*xp/(P.g*x);
     % Law of motion for capital (15)
-    kp = (1-P.delta)*(k/g)+xp*(1-(xg-1)^2/2);         
+    kp = (1-P.delta)*(k/g)+xp*(1-P.nu*(xg-1)^2/2);         
     %----------------------------------------------------------------------
     % Linear interpolation of the policy functions 
     %---------------------------------------------------------------------- 
-    [cppArr3,pigappArr3,npArr3,qpArr3] = Fallterp743_R(...
+    [cppArr3,pigappArr3,npArr3,qpArr3,upspArr3] = Fallterp853_R(...
         O.g_pts,O.s_pts,O.mp_pts,...
         O.in_pts,O.c_pts,O.k_pts,O.x_pts,O.w_pts,...
-        G.in_grid,G.c_grid,G.k_grid,G.x_grid,...
-        inp,cp,kp,xp,...
-        pf.c,pf.pigap,pf.n,pf.q);
+        G.in_grid,G.c_grid,G.k_grid,G.x_grid,G.w_grid,...
+        inp,cp,kp,xp,wp,...
+        pf.c,pf.pigap,pf.n,pf.q,pf.ups);
     %----------------------------------------------------------------------        
     % Next period
     %----------------------------------------------------------------------  
     % HH FOC Utilization (1)
-    %rkpArr3 = S.rk*exp(P.sigups*(upspArr3-1));  
+    rkpArr3 = S.rk*exp(P.sigups*(upspArr3-1));  
     % Production function (2)
-    yfpArr3 = (kp./gpArr3).^P.alpha.*npArr3.^(1-P.alpha);
+    yfpArr3 = (upspArr3*kp./gpArr3).^P.alpha.*npArr3.^(1-P.alpha);
     % Utilization definition (3)
-    %upArr3 = S.rk*(exp(P.sigups*(upspArr3-1))-1)/P.sigups;
+    upArr3 = S.rk*(exp(P.sigups*(upspArr3-1))-1)/P.sigups;
     % Firm FOC capital (4)
-    mcpArr3 = rkpArr3.*kp./(P.alpha*gpArr3.*yfpArr3);
+    mcpArr3 = rkpArr3.*upspArr3*kp./(P.alpha*gpArr3.*yfpArr3);
     % Firm FOC labor (5)
     wppArr3 = (1-P.alpha)*mcpArr3.*yfpArr3./npArr3;
     % Real wage growth gap (6)
     wgpArr3 = pigappArr3.*gpArr3.*wppArr3/(P.g*wp);
     % Output definition (7)
-    yppArr3 = (1-P.varphip*(pigappArr3-1).^2/2).*yfpArr3;
+    yppArr3 = (1-P.varphip*(pigappArr3-1).^2/2 ...
+        - P.varphiw*(wgpArr3-1).^2/2).*yfpArr3 ...
+        - upArr3*kp./gpArr3;
     % Inverse MUC (11)
     lampArr3 = cppArr3-P.h*cp./gpArr3;
     % ARC (13)
@@ -95,15 +97,15 @@ for icol = 1:ncol
     % Expectations
     %----------------------------------------------------------------------
     EbondArr3 = weightArr3.*sdfArr3./(gpArr3.*(P.pi*pigappArr3));
-    EcapArr3 = weightArr3.*sdfArr3.*(rkpArr3+(1-P.delta)*qpArr3)./gpArr3;
+    EcapArr3 = weightArr3.*sdfArr3.*(rkpArr3.*upspArr3-upArr3+(1-P.delta)*qpArr3)./gpArr3;
     EinvArr3 = weightArr3.*sdfArr3.*qpArr3.*xgpArr3.^2.*(xgpArr3-1)./gpArr3;
     EppcArr3 = weightArr3.*sdfArr3.*(pigappArr3-1).*pigappArr3.*(yfpArr3/yf);
-    %EwpcArr3 = weightArr3.*sdfArr3.*(wgpArr3-1).*wgpArr3.*(yfpArr3/yf);
+    EwpcArr3 = weightArr3.*sdfArr3.*(wgpArr3-1).*wgpArr3.*(yfpArr3/yf);
     Ebond = sum(EbondArr3(:));
     Ecap = sum(EcapArr3(:));
     Einv = sum(EinvArr3(:));
     Eppc = sum(EppcArr3(:));
-    %Ewpc = sum(EwpcArr3(:));
+    Ewpc = sum(EwpcArr3(:));
     %----------------------------------------------------------------------
     % Euler Equations
     %----------------------------------------------------------------------
@@ -112,9 +114,9 @@ for icol = 1:ncol
     % HH FOC capital (17)
     Res(2,icol) = q-Ecap;
     % HH FOC investment (18)
-    Res(3,icol) = 1-q*(1-(xg-1)^2/2-(xg-1)*xg)-P.g*Einv;    
+    Res(3,icol) = 1-q*(1-P.nu*(xg-1)^2/2-P.nu*(xg-1)*xg)-P.nu*P.g*Einv;    
     % Price Phillips Curve (19)
-    Res(4,icol) = P.varphi*(pigap-1)*pigap-(1-P.theta)-P.theta*mc-P.varphi*Eppc;
+    Res(4,icol) = P.varphip*(pigap-1)*pigap-(1-P.thetap)-P.thetap*mc-P.varphip*Eppc;
     % Wage Phillips Curve (20)
-    %Res(5,icol) = P.varphiw*(wg-1)*wg-((1-P.thetaw)*wp+P.thetaw*wf)*n/yf-P.varphiw*Ewpc;
+    Res(5,icol) = P.varphiw*(wg-1)*wg-((1-P.thetaw)*wp+P.thetaw*wf)*n/yf-P.varphiw*Ewpc;
 end
